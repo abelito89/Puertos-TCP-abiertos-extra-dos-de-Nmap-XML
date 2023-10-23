2ddbf5df98259a637e810b256f94d2d898d97e7b
Abel Gómez Méndez <abelmetaltele@gmail.com>
2023-10-23 06:18:22 -0400

    commit de carga del xml

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import streamlit as st
+import sys
+sys.path.append("C:\\Abel\\Trabajo\Proyectos Ciencia de Datos\\XML to txt Puertos TCP Nmap\\src")
+import xml_to_txt
+
+def main():
+    st.title('Cargador de archivos XML')
+
+    # Solicitar la carga del archivo XML
+    uploaded_file = st.file_uploader("Por favor, sube un archivo XML", type="xml")
+    
+    if uploaded_file is not None:
+        st.success('Archivo cargado exitosamente!')
+        xml_to_txt.xml_to_txt(uploaded_file)
+
+    
+if __name__ == "__main__":
+    main()
--- src/xml_to_txt.py
+++ src/xml_to_txt.py
@@ -1,16 +1,17 @@
 import xml.etree.ElementTree as ET
 
-def xml_to_txt(xml_file, txt_file):
+def xml_to_txt(xml_file):
     # Parsear el archivo XML
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
-    # Crear o abrir el archivo TXT
-    with open(txt_file, 'w') as f:
-        # Recorrer el XML y escribir en el TXT
-        for elem in root.iter():
-            if elem.text:
-                f.write(elem.text + '\n')
+    # Crear una lista para almacenar las líneas de texto
+    lines = []
 
-# Uso de la función
-xml_to_txt('/content/Subred63.xml', 'Subred63.txt')
+    # Recorrer el XML y añadir a la lista
+    for elem in root.iter():
+        if elem.text:
+            lines.append(elem.text)
+
+    # Unir las líneas con saltos de línea y devolver
+    return '\n'.join(lines)
